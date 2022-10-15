8ae98ed01f22b83e993061e3f8aed22fbcff3c5e
fuwb <962920136@qq.com>
2022-10-15 20:26:20 +0800

    第三次提交

---
+++ vue/vue_basic/19_单文件组件/Student.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="demo">
+        <!-- 组件的结构 -->
+        <h2>学生名称：{{ studentlName }}</h2>
+        <h2>年龄：{{ age }}</h2>
+    </div>
+</template>
+
+<script>
+    // 组件交互相关的代码(数据、方法)
+    export default {
+        name: 'School',
+        data() {
+            return {
+                studentlName: '小爱同学',
+                age: 19
+            }
+        },
+    }
+</script>
+
+<style>
+/* 组件样式 */
+</style>
